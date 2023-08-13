52599959582e49ed2f0d6f00094584b224d038dc
ashish31033 <142102244+ashish31033@users.noreply.github.com>
2023-08-13 20:41:30 +0530

    chocolatestack.c

--- chocolatestack.c
+++ chocolatestack.c
@@ -1,25 +1,74 @@
-#include<stdio.h>
-#include<stdbool.h>
-#include<malloc.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int* solution (int N, int* C, int* out_n) {
-    // Write your code here
-    // Assign the size of the array to be returned into out_n variable
-    // *out_n = array_size;
-    
+#define MAX_N 100
+
+typedef struct {
+    int chocolates[MAX_N];
+    int top;
+} Stack;
+
+void initializeStack(Stack *stack) {
+    stack->top = -1;
+}
+
+int isEmpty(Stack *stack) {
+    return stack->top == -1;
+}
+
+void push(Stack *stack, int chocolates) {
+    if (stack->top < MAX_N - 1) {
+        stack->top++;
+        stack->chocolates[stack->top] = chocolates;
+    } else {
+        printf("Stack is full, cannot add more chocolates.\n");
+    }
+}
+
+int pop(Stack *stack) {
+    if (!isEmpty(stack)) {
+        int chocolates = stack->chocolates[stack->top];
+        stack->top--;
+        return chocolates;
+    } else {
+        printf("Stack is empty, cannot sell chocolates.\n");
+        return -1;
+    }
 }
 
 int main() {
-    int out_n;
-    int N;
-    scanf("%d", &N);
-    int i_C;
-    int *C = (int *)malloc(sizeof(int)*(N));
-    for(i_C = 0; i_C < N; i_C++)
-    	scanf("%d", &C[i_C]);
+    Stack stack;
+    initializeStack(&stack);
 
-    int* out_ = solution(N, C, &out_n);
-    printf("%d", out_[0]);
-    for(int i_out_ = 1; i_out_ < out_n; i_out_++)
-    	printf(" %d", out_[i_out_]);
+    int N;
+    printf("Enter the number of minutes: ");
+    scanf("%d", &N);
+
+    for (int i = 1; i <= N; i++) {
+        int choice, chocolates;
+        printf("Minute %d:\n", i);
+        printf("1. Sell chocolates\n");
+        printf("2. Receive chocolates from warehouse\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        if (choice == 1) {
+            if (!isEmpty(&stack)) {
+                chocolates = pop(&stack);
+                printf("Sold a box with %d chocolates.\n", chocolates);
+            } else {
+                printf("No chocolates to sell.\n");
+            }
+        } else if (choice == 2) {
+            printf("Enter the number of chocolates in the box: ");
+            scanf("%d", &chocolates);
+            push(&stack, chocolates);
+            printf("Received a box with %d chocolates from warehouse.\n", chocolates);
+        } else {
+            printf("Invalid choice.\n");
+            i--;  // Decrement the loop variable to repeat the current minute.
+        }
+    }
+
+    return 0;
 }
